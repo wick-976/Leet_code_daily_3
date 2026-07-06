723d9822726e03b27c25db62d65665ea9c8d54fb
wick-976 <cbtharan976@gmail.com>
2026-07-06 12:12:51 +0530

    maximum Sub array

---
+++ maximum_sub_array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int sum=0;
+        int best=nums[0];
+        
+
+        for(int i=0;i<nums.size();i++)
+        {
+            sum=max(nums[i],sum+nums[i]);
+            best=max(best,sum);
+        }
+
+        return best;
+    }
+};
