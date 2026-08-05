4a52356c58171da57e7200de376288678fa4d49d
wick-976 <cbtharan976@gmail.com>
2026-08-05 21:57:28 +0530

    Q1. Set Mismatch

---
+++ Q1. Set Mismatch.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> count(n+1, 0);
+        int duplicate = -1, missing = -1;
+
+        // Count occurrences
+        for (int num : nums) {
+            count[num]++;
+        }
+
+        // Find duplicate and missing
+        for (int i = 1; i <= n; i++) {
+            if (count[i] == 2) duplicate = i;
+            if (count[i] == 0) missing = i;
+        }
+
+        return {duplicate, missing};
+    }
+};
