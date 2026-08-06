7452943bb3cf256e3b3cea82657549c8b8cfbe80
wick-976 <cbtharan976@gmail.com>
2026-08-06 22:00:16 +0530

    Rotation array

--- 189.Rotate_array.cpp
+++ 189.Rotate_array.cpp
@@ -1,3 +1,62 @@
+//better solution
+/*#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
+
+    int size = arr.size();
+
+    // Check if the array is empty
+    if (arr.empty())
+    {
+        cout << "Array is empty!";
+        return 0;
+    }
+
+    // Get the number of rotations
+    int k;
+    cout << "Enter the value of k: ";
+    cin >> k;
+
+    // Calculate effective rotations
+    k = k % size;
+
+    // Temporary array to store the last k elements
+    vector<int> temp_array;
+
+    for (int i = size - k; i < size; i++)
+    {
+        temp_array.push_back(arr[i]);
+    }
+
+    // Shift the remaining elements to the right
+    for (int i = size - k - 1; i >= 0; i--)
+    {
+        arr[i + k] = arr[i];
+    }
+
+    // Copy the saved elements to the beginning
+    for (int i = 0; i < k; i++)
+    {
+        arr[i] = temp_array[i];
+    }
+
+    // Print the rotated array
+    cout << "Rotated Array: ";
+    for (int x : arr)
+    {
+        cout << x << " ";
+    }
+
+    return 0;
+}*/
+
+
+
+
+//optimal solution =>>
 class Solution {
 public:
 
--- max_consecuitive_ones.cpp
+++ max_consecuitive_ones.cpp
@@ -12,7 +12,6 @@ public:
                 count = 0;
             }
         }
-
         return maxi;
     }
 };
