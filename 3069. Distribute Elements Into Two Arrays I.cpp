5a5f1c4b5a84c757b90b0c6f0a997d4e92238fe6
wick-976 <cbtharan976@gmail.com>
2026-08-20 22:05:09 +0530

    3069. Distribute Elements Into Two Arrays I

---
+++ 3069. Distribute Elements Into Two Arrays I.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int> arr1, arr2;
+
+
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+
+   
+        for (int i = 2; i < nums.size(); i++) {
+            if (arr1.back() > arr2.back()) {
+                arr1.push_back(nums[i]);
+            } else if (arr1.back() < arr2.back()) {
+                arr2.push_back(nums[i]);
+            } else {
+                
+                if (arr1.size() <= arr2.size()) {
+                    arr1.push_back(nums[i]);
+                } else {
+                    arr2.push_back(nums[i]);
+                }
+            }
+        }
+
+        vector<int> result = arr1;
+        result.insert(result.end(), arr2.begin(), arr2.end());
+        return result;
+    }
+};
